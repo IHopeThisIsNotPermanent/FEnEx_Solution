f82e0bdb800b71db9963e0ff3623172421a3dc5b
Saul Rathbone-Boschis <Saul.rathbos@gmail.com>
2022-08-09 14:18:37 +1000

    Update paralleloperation.py
    Continued working on the paralleloperation.py

--- paralleloperation.py
+++ paralleloperation.py
@@ -58,16 +58,138 @@ class FailureCount:
     def __init__(self, choose = "Set", params = None):
         if params is None:
             if choose == "Set":
-                params = 1
+                params = 2
         
-        if choose == "set":
+        if choose == "Set":
             self.sample_func = lambda : params
         
         
     def sample(self):
         return self.sample_func()
     
+class FailSegment:
+    def __init__(self, start, end, value):
+        self.start = start
+        self.end = end
+        self.value = value
     
+    def __le__(self, other):
+        return self.start <= other.start
+    
+    def __lt__(self, other):
+        return self.start < other.start
+    
+    def __str__(self):
+        return "(" + str(self.start) + ", " + str(self.end) + ", " + str(self.value) + ")"
+    
+    def collide(self, other):
+        return max(self.start, other.start) < min(self.end, other.end)
+    
+    def compose(self, other):
+        """
+        
+
+        Parameters
+        ----------
+        other : FailSegment
+            The other fail segment you wish to compose this one with.
+
+        Returns
+        -------
+        tuple<FailSegment>
+            the failsegments in reverse order, after a compose
+
+        """
+        values = [0,self.value + other.value,0]
+        
+        if self.start < other.start:
+            values[0] = self.value
+        if self.start > other.start:
+            values[0] = other.value
+            
+        if self.end < other.end:
+            values[2] = other.value
+        if self.end > other.end:
+            values[2] = self.value
+        
+        return (FailSegment(min(self.end, other.end), max(self.end, other.end), values[2]),
+                FailSegment(max(self.start, other.start), min(self.end, other.end), values[1]),
+                FailSegment(min(self.start, other.start), max(self.start, other.start), values[0]))
+                
+    
+    def merge(self, other):
+        return FailSegment(min(self.start, other.start), max(self.end, other.end), self.value)
+    
+class Zero:
+    def __init__(self):
+        pass
+    def __lt__(self, other):
+        return False
+    
+    def __gt__(self, other):
+        return True
+    
+class SegmentGraph:
+    def __init__(self):
+        self.vals = []
+        self.buff = []
+        self.integral = 0
+        
+    def buffer(self, values):
+        self.buff += values
+        
+    def add(self, value):
+        self.buff.append(value)
+        
+    def update(self):
+        T1 = time.time()
+        if len(self.buff) == 0:
+            return
+        self.vals = [0, ] * len(self.buff)*2
+        firsts = list(np.sort([x.start for x in self.buff]))
+        seconds = list(np.sort([x.end for x in self.buff]))
+        print("sort:" , time.time() - T1)
+        T1 = time.time()
+        vals_index = 0
+        firsts_index = 0
+        seconds_index = 0
+        count = 0
+        self.integral = 0
+        while firsts_index < len(firsts):
+            
+            if vals_index == len(self.vals):
+                self.vals += [0,] * len(self.vals)
+                
+            if firsts[firsts_index] < seconds[seconds_index]:
+                count += 1
+                self.vals[vals_index] = (firsts[firsts_index], count)
+                firsts_index += 1
+            elif firsts[firsts_index] > seconds[seconds_index]:
+                count -= 1
+                self.vals[vals_index] = (seconds[seconds_index], count)
+                seconds_index += 1
+            elif firsts[firsts_index] == seconds[seconds_index]:
+                seconds_index += 1
+                firsts_index += 1
+                
+            vals_index += 1
+        
+        while seconds_index < len(seconds):
+            
+            if vals_index == len(self.vals):
+                self.vals += [0,] * len(self.vals)
+            
+            count -= 1
+            self.vals[vals_index] = (seconds[seconds_index], count)
+            seconds_index += 1
+            
+            vals_index += 1
+            
+        print("indexing:", time.time() - T1)
+            
+        
+        
+
 class ParallelOperation:
     def __init__(self, n_comps, comp_contribution, comp_function = "Weibull", 
                  comp_params = None, comp_failcount = "Set", comp_failparams = None,
@@ -119,27 +241,65 @@ class ParallelOperation:
         if comp_params is None:
             comp_params = [None,]*n_comps
         if comp_failparams is None:
-            comp_failcount = [None,]*n_comps
+            comp_failparams = [None,]*n_comps
         self.comp_functions = [FailureFunction(comp_function, params) for params in comp_params]
-        self.comp_failcount = [FailureCount(comp_failcount, params) for params in comp_failcount]
+        self.comp_failcount = [FailureCount(comp_failcount, params) for params in comp_failparams]
+        self.ttf = ttf
         
-        self.data = [[] for x in range(n_comps)] #self.data[0] if you fit linear segments to the data points, it will
+        
+        self.data = [SegmentGraph() for x in range(n_comps)] #self.data[0] if you fit linear segments to the data points, it will
                                                  #             tell you the number of times only 1 failure was occuing
                                                  #self.data[1] will be 2 failures, ect...
                                                  
         self.sorted = False
         self.arranged_data = [] #the y values of all the aligned x_values
         
-    def simulate(self, n = 100000, timeit = False): #probs make quicker
+    def simulate(self, n = 10000, timeit = False): #probs make quicker
         if timeit:
             T1 = time.time()
         
-        self.sorted = False
         for i in range(n):
-            nxt = [[self.comp_functions[x].sample() for y in range(self.comp_failcount[x].sample())] for x in range(self.n_comps)]
-            nxt.sort()
-            for k in range(self.n_comps):
-                self.data[k].append(nxt[k])
+            fail_segments = []
+            for comp_index in range(self.n_comps):
+                fail_count = self.comp_failcount[comp_index].sample()
+                nxt = [0,] * fail_count
+                for r in range(fail_count):
+                    start = self.comp_functions[comp_index].sample()
+                    nxt[r] = FailSegment(start, start + self.ttf, 1)
+               
+                nxt.sort()
+                
+                index = 0
+                while index < len(nxt)-1:
+                    if nxt[index].collide(nxt[index+1]):
+                        merged = nxt[index].merge(nxt[index+1])
+                        del nxt[index]
+                        del nxt[index]
+                        nxt.insert(index, merged)
+                    else:
+                        index += 1
+                fail_segments = fail_segments + nxt
+            
+            fail_segments.sort()
+            
+            index = 0
+            while index < len(fail_segments)-1:
+                if fail_segments[index].collide(fail_segments[index+1]):
+                    add_values = fail_segments[index].compose(fail_segments[index+1])
+                    del fail_segments[index]
+                    del fail_segments[index]
+                    for add in add_values:
+                        if add.value != 0:
+                            fail_segments.insert(index, add)
+                else:
+                    index += 1
+                    
+            for segment in fail_segments:
+                self.data[segment.value-1].add(segment)
+            
+        for dat in self.data:
+            dat.update()
+            
         if timeit:
             print(n, " Iterations in ", time.time() - T1, " seconds.")
         
@@ -150,60 +310,21 @@ class ParallelOperation:
         pass
     
     def summarise(self):
-        
-        y_vals = list(np.arange(0,1,1/(len(self.data[0])+2)))
-        
-        most = max([max(x) for x in self.data])
-        least = min([min(x) for x in self.data])
-        
-        if not self.sorted:
-            self.sorted = True
-            for x in self.data:
-                x.sort()
-            for x in self.data:
-                x.insert(0,least)
-                x.append(most)
-            
-            self.arranged_data = []
-            for comp in range(self.n_comps):
-                self.arranged_data.append(linfunc.linsample(self.data[comp], y_vals, (least, most, len(self.data[0]))))
-            
-        
-        #Overlayed probabilitys of failure  
-          
-        plt.title("Cumulative probability of failures overlayed")
-        plt.xlabel("Time of failure")
+        plt.title("Probability of n-failures")
+        plt.xlabel("no. failures")
         plt.ylabel("Probability as a percent")
-        count = 1
-        for x_vals in self.data:
-            plt.plot(x_vals, y_vals, label = str(count) + " Failures")
-            count += 1
-        plt.legend()
+        for n in range(n_comps):
+            
+            plt.bar(list(range(0,self.components[0]+1)), [100*x/sum(self.data_points) for x in self.data_points])
         plt.show()
         plt.figure()
-
-        #Predicted efficency over time
-        
-        plt.title("Predicted efficency over time")
-        plt.xlabel("Time")
-        plt.ylabel("Efficency as a percent")
-        
-        x_vals = np.arange(least, most, (most-least)/(len(self.data[0])))
-        y_vals = []
-        
-        for x in range(len(x_vals)):
-            total = self.arranged_data[self.n_comps-1][x]*self.iof(self.n_comps)
-            for comp in range(self.n_comps-1):
-                total += (self.arranged_data[comp][x]-self.arranged_data[comp+1][x])*(self.iof(comp+1))
-            y_vals.append(100*(1-total))
-        
-        plt.plot(x_vals, y_vals)
         
         
 if __name__ == "__main__":
     Test1 = ParallelOperation(4,1/3, comp_function = "Weibull")
     Test1.simulate(timeit = True)
-    Test1.summarise()
+    #Test1.summarise()
+    pass
         
         
         
